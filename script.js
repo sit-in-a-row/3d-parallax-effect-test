b83d78df816bc706b3ef3dcd8d6b365ead275150
sit-in-a-row <145440823+sit-in-a-row@users.noreply.github.com>
2024-02-14 14:44:17 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById('gridContainer').addEventListener('mousemove', function(e) {
+    const rect = this.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = (y - centerY) / 10; // Y축 기준 회전 각도
+    const rotateY = (x - centerX) / 10; // X축 기준 회전 각도
+
+    for (let i = 1; i <= 5; i++) {
+        const grid = document.getElementById(`grid${i}`);
+        const depth = 20; // 회전 깊이 조절을 위한 변수, 필요에 따라 조절
+        grid.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateZ(${depth}px)`;
+    }
+});
+
+// gridContainer에서 마우스가 벗어났을 때 회전 효과 초기화
+document.getElementById('gridContainer').addEventListener('mouseleave', function() {
+    for (let i = 1; i <= 5; i++) {
+        const grid = document.getElementById(`grid${i}`);
+        grid.style.transform = 'none';
+    }
+});
